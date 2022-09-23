33caf91af179f96cd8eaa12392310c20cd5db8bf
dasom222g <dasom228@gmail.com>
2022-09-23 16:41:52 +0900

    Add contract logic

--- contracts/StorageUpgrade.sol
+++ contracts/StorageUpgrade.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.9;
 
 contract StorageUpgrade {
-  
-}
+  uint256 storedData;
+
+  event Chage(string message, uint256 newValue);
+
+  function get() public view returns (uint256) {
+    return storedData;
+  }
+
+  function set(uint256 x) public {
+    require(x < 5000, 'Should be less than 5000');
+    storedData = x;
+    emit Chage('set', x);
+  }
+} 
